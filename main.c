1f1b9eb98375c750049f50e1c090e002b7e9d26f
Lucas Cid Duarte <128184251+LucasCidi@users.noreply.github.com>
2024-11-14 18:34:21 +0000

    pronto

--- main.c
+++ main.c
@@ -1,41 +1,28 @@
 #include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#define MAX_ARQUIVOS 100
 
-typedef struct {
-    char nome[100];
-    int permissao;
-} Arquivo;
-
-void exibirPermissoes(int permissao, const char *nome_arquivo) {
+void exibirPermissoes(int permissao, const char *arquivo) {
     char permissoes[10];
-    permissoes[0] = (permissao & 0400) ? 'r' : '-'; // Proprietário: leitura
-    permissoes[1] = (permissao & 0200) ? 'w' : '-'; // Proprietário: escrita
-    permissoes[2] = (permissao & 0100) ? 'x' : '-'; // Proprietário: execução
-    permissoes[3] = (permissao & 0040) ? 'r' : '-'; // Grupo: leitura
-    permissoes[4] = (permissao & 0020) ? 'w' : '-'; // Grupo: escrita
-    permissoes[5] = (permissao & 0010) ? 'x' : '-'; // Grupo: execução
-    permissoes[6] = (permissao & 0004) ? 'r' : '-'; // Outros: leitura
-    permissoes[7] = (permissao & 0002) ? 'w' : '-'; // Outros: escrita
-    permissoes[8] = (permissao & 0001) ? 'x' : '-'; // Outros: execução
+    permissoes[0] = (permissao & 0400) ? 'r' : '-'; 
+    permissoes[1] = (permissao & 0200) ? 'w' : '-'; 
+    permissoes[2] = (permissao & 0100) ? 'x' : '-';
+    permissoes[3] = (permissao & 0040) ? 'r' : '-'; 
+    permissoes[4] = (permissao & 0020) ? 'w' : '-'; 
+    permissoes[5] = (permissao & 0010) ? 'x' : '-';
+    permissoes[6] = (permissao & 0004) ? 'r' : '-';
+    permissoes[7] = (permissao & 0002) ? 'w' : '-'; 
+    permissoes[8] = (permissao & 0001) ? 'x' : '-';
 
-    printf("-%s %s\n", permissoes, nome_arquivo);
+    printf("-%s %s\n", permissoes, arquivo);
 }
 
 int main() {
     int permissao;
-    char arquivo[100];
-    int sair;
 
     while (1) {
-        printf("Digite as permissoes: ");
+        printf("Digite as permissoes em octal: ");
         scanf("%o", &permissao);
 
-        printf("Digite o nome do arquivo: ");
-        scanf("%s", arquivo);
-
-        exibirPermissoes(permissao, arquivo);
+        exibirPermissoes(permissao, "arquivo.txt");
     }
 
     return 0;
